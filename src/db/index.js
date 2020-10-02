8f4db1652e49c642b933ec4a000066244be0eb39
Zipexpo <nvtngan@hcmus.edu.vn>
2020-10-02 15:28:26 -0500

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ const csv = require('csv-parser');
 const studentDB = require('./students')
 
 const uri = require('../../config/keys').mongoURI;
-const dbName = 'CS5331-VirtualReality-Fall2020'
+const dbName = 'CS5331-VirtualReality-Fall2020-p1'
 let client = null
 let studentConn = null
 let scoreConn = null
