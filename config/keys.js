8e1f04b1df575418f46c50b526e615a4c18f3a32
Zipexpo <nvtngan@hcmus.edu.vn>
2021-02-28 23:16:11 -0600

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI:"mongodb+srv://idvl:MwckVolsbWHnaOhy@cluster0.h6or9.mongodb.net/CS5331-VirtualReality-Fall2020-p1?retryWrites=true&w=majority"
+    mongoURI:"mongodb+srv://idvl:MwckVolsbWHnaOhy@cluster0.h6or9.mongodb.net/HCI-Spring-2021?retryWrites=true&w=majority"
 };
